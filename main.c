81e3bcf7886cffcee410a204431d7ff5717fc05e
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2023-07-28 04:54:29 -0700

    Handel exit code of chiled process

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <string.h>
-
+#include <errno.h>
 /**
  * execute_command - Execute a command in a child process.
  * @command: The command to be executed.
@@ -14,10 +14,8 @@
 void execute_command(char *command, char *pname)
 {
 	pid_t pid;
-	char *environ[] = { (char *) "PATH=/bin", 0};
-	char *token;
-	char *argv[64];
-	int i = 0;
+	char *environ[] = { (char *) "PATH=/bin", 0}, *token, *argv[64];
+	int status, wait_result, i = 0;
 
 	token = strtok(command, " ");
 	if (token == NULL)
@@ -30,10 +28,7 @@ void execute_command(char *command, char *pname)
 	argv[i] = NULL;
 	pid = fork();
 	if (pid == -1)
-	{
-		perror("Error");
-		return;
-	}
+		exit(1);
 	if (pid == 0)
 	{
 		if (execve(argv[0], argv, environ) == -1)
@@ -44,7 +39,14 @@ void execute_command(char *command, char *pname)
 	}
 	else
 	{
-		wait(NULL);
+		do
+			wait_result = 	waitpid(pid, &status, 0);
+		while (wait_result == -1 && errno == EINTR);
+
+		if (wait_result == -1)
+			exit(1);
+		else
+			exit(status >> 8);
 	}
 }
 /**
