7e6d6883c63b8854b05b14f4f3df86955e785601
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2023-07-28 00:39:44 -0700

    Implement the exit built-in, that exits the shell

--- main.c
+++ main.c
@@ -45,7 +45,47 @@ void execute_command(char *command, char *pname)
 		wait(NULL);
 	}
 }
+/**
+ *  _strcmp - function that compares two strings.
+ * @s1: the pointer to first string.
+ * @s2: the pointer to second  string.
+ * Return: diffrens between the strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int d = 0;
+	int c1 = 0;
+	int c2 = 0;
+	int s = 1;
+	char *p1 = s1;
+	char *p2 = s2;
 
+	while (*(p1 + c1) != '\0')
+	{
+		c1++;
+	}
+	while (*(p2 + c2) != '\0')
+	{
+		c2++;
+	}
+	if (c2 > c1)
+	{
+		p1 = s2;
+		p2 = s1;
+		s = -1;
+	}
+	while (*p1 != '\0')
+	{
+		d = (int)*p1 - (int)*p2;
+		p1++;
+		p2++;
+		if (d != 0)
+		{
+			return (d * s);
+		}
+	}
+return (d * s);
+}
 /**
  * main - Simple shell
  * @av : av is a NULL terminated array of strings.
@@ -72,18 +112,13 @@ int main(int ac, char **av)
 		read = getline(&line, &len, stdin);
 
 		if (read == -1)
-		{
 			break;
-		}
 		else if (read == 1 && line[0] == '\n')
-		{
 			continue;
-		}
 		if (line[read - 1] == '\n')
-		{
 			line[read - 1] = '\0';
-		}
-
+		if (_strcmp(line, "exit") == 0)
+			break;
 		if (ac >= 0 && line != NULL)
 			execute_command(line, av[0]);
 
